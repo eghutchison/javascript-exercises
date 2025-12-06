09d0b475eb1d31c29f8ac86fe9e842926483e3c0
Eric Hutchison <79342284+eghutchison@users.noreply.github.com>
2025-12-06 09:11:51 -1000

    Solution to Ex 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(word) {
+    let newWord="";
+    let wordLength = word.length;
+    for (let i=wordLength - 1; i >= 0; i--){
+        newWord+=word.charAt(i);
+        }
+        return newWord;
+    };
 
 // Do not edit below this line
 module.exports = reverseString;
