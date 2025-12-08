556df81d0e662dbc851c94be732965057065b308
Eric Hutchison <79342284+eghutchison@users.noreply.github.com>
2025-12-08 13:28:22 -1000

    Solution for Ex 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...rem) {
+    let result = arr.filter (element => !rem.includes(element));
+    return result;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
+//     for(arrs in arr) {
+//         for (let rems of rem) {
+//             if (rem === arr) {
+//                 removeFromArray.pop[]
+//             }
+//         }  
+//     }  
+// };
