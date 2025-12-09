c9dfb2689e5d02153e31ff1ef81ecc6f6ea8dfa2
Eric Hutchison <79342284+eghutchison@users.noreply.github.com>
2025-12-09 12:46:16 -1000

    Solution to ex 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (a < 0 || b < 0) {return "ERROR"};
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {return "ERROR"};
+    let start = Math.min(a,b);
+    let end = Math.max(a,b);
+    let arr = [];
+    for (let i = start; i <= end; i++) {
+        arr.push(i);
+    }
+    return arr.reduce((sum,current) => sum + current,0);
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
