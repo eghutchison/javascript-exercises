04269663afe606c8e2949bb2f95a4f356a459666
Eric Hutchison <79342284+eghutchison@users.noreply.github.com>
2025-12-09 13:12:02 -1000

    Solution to ex 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(yr) {
+    if (yr % 4 === 0 && (yr % 100 != 0 || yr % 400 === 0)) {
+        return true;
+    };
+    return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// perform action on argument to determine if leap year === true
+// if arg / 4 === true > arg / 100 > arg / 400
